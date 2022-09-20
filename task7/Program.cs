701507f53a33a2da92a2c696244d3527df813827
Dmitriy <dmitriy.shehobalov@gmail.com>
2022-09-20 11:00:45 +0300

    Add task7

---
+++ task7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число, а на выходе показывает последнюю цифру этого числа
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 10;
+Console.Write($"Third number {number} = {result} ");
