545594200248a6a1c7b709c951f2d5275db867dc
Dmitriy <dmitriy.shehobalov@gmail.com>
2022-09-20 10:54:58 +0300

    Add task5

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход получает число N, а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Enter integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = -number;
+while(i<=number){
+    Console.Write(i+" " );
+    i++;
+}
